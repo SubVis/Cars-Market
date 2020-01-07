cf0089025b3195c98767637b82f5ec10469db928
MohammedAyman2018 <emidobibo20@gmail.com>
2020-01-08 01:29:37 +0200

    Update get forms values automatically function to get selections and textareas

--- public/market/js/axios.js
+++ public/market/js/axios.js
@@ -15,7 +15,6 @@ function getFormVal(forms){
         inputs.forEach(npt => {
             /** create/reset a temporary array to store the name and value for the current input */
             let temp = [];
-            console.log(npt);
             /** adding the values and the name of the input to the temp array */
             temp.push(npt.value,npt.getAttribute("name"));
             /** Add the temp to the value array to be returned*/
@@ -24,18 +23,36 @@ function getFormVal(forms){
         
         /** check if there is textareas and get their values */
         if (form.getElementsByTagName('textarea')){
+            /** get all textareas in the form*/
             let textareas = [...form.getElementsByTagName("textarea")];
-
+            /** loop throgh them */
             textareas.forEach(txtarea => {
+                /** create/reset a temporary array to store the name and value for the current input */
                 let temp = [];
-                
+                /** adding the values and the name of the textarea to the temp array */
                 temp.push(txtarea.value, txtarea.getAttribute("name"));
+                /** Add the temp to the value array to be returned*/
+                values.push(temp);
+            });
+        };
+
+        /** check if there is selections and get their values */
+        if (form.getElementsByTagName('select')){
+            /** get all selections in the form*/
+            let selections = [...form.getElementsByTagName("select")];
+            /** loop throgh them */
+            selections.forEach(Selection => {
+                let temp = [];
+                /** create/reset a temporary array to store the name and value for the current input */
+                let options = Selection.options[Selection.selectedIndex].value;
+                /** adding the values and the name of the textarea to the temp array */
+                temp.push(options, Selection.getAttribute("name"));
+                /** Add the temp to the value array to be returned*/
                 values.push(temp);
             });
         };
         /** End if */
     });
-    console.log(values);
     return values;
 };
 
@@ -44,8 +61,8 @@ var paragraph= [...document.querySelectorAll("[data-validation]")];
 
 document.getElementById("car-store").addEventListener("click", function(event){
     event.preventDefault();
-    getFormVal([...document.getElementsByTagName('form')]);
-
+    let formVal = getFormVal([...document.getElementsByTagName('form')]);
+    console.log('form=', formVal)
     axios.post('storeCars').then(function (response) {
         /** handle success */
 
