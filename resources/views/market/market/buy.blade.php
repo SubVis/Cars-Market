e6001661cf920a49e36c279426153350cc9e547c
MohammedAyman2018 <emidobibo20@gmail.com>
2020-02-08 18:03:11 +0200

    Remove some useless files

--- resources/views/market/market/buy.blade.php
+++ resources/views/market/market/buy.blade.php
@@ -55,7 +55,7 @@
                 </div>
             </div> 
             <!-- End Car Images -->
-            
+
             <!-- Start Car Details -->
             <section class="card-details border border-balck px-2 pt-2 flex flex-wrap">
                 <h2 class="color font-bold inline-block text-right my-4 mb-2 w-full text-xl">{{$car->title}} </h2>
@@ -113,30 +113,43 @@
     @endforeach
 </div>
 
+<!-- Start Pagenation -->
 <div class="flex justify-center my-6 pagenation">
     {{$cars->links()}}
     <ul class="flex border border-grey">
-        <li><a class="block px-3 py-2 color hover:text-black " href="#">السابق</a></li>
-        <li><a class="block border-r border-grey-light px-3 py-2 color  active hover:text-black" href="#">1</a></li>
-        <li><a class="block border-r border-grey-light px-3 py-2 color hover:text-black" href="#">2</a></li>
-        <li><a class="block text-white bg-blue px-3 py-2 color border-r border-grey-light hover:text-black"
-                href="#">3</a></li>
-        <li><a class="block px-3 py-2 color border-r border-grey hover:text-black" href="#">التالي</a></li>
+        <li>
+            <a class="block px-3 py-2 color hover:text-black " href="#">السابق</a>
+        </li>
+        <li>
+            <a class="block border-r border-grey-light px-3 py-2 color  active hover:text-black" href="#">1</a>
+        </li>
+        <li>
+            <a class="block border-r border-grey-light px-3 py-2 color hover:text-black" href="#">2</a>
+        </li>
+        <li>
+            <a class="block text-white bg-blue px-3 py-2 color border-r border-grey-light hover:text-black" href="#">3</a>
+        </li>
+        <li>
+            <a class="block px-3 py-2 color border-r border-grey hover:text-black" href="#">التالي</a>
+        </li>
     </ul>
 </div>
+<!-- End Pagenation -->
+
 <div class="shape flex justify-center w-full overflow-x-hidden">
     <div class="all" style="transform: rotate(-6deg)">
         <p class="text-center mb-4">عايز تبيع</p><a class="btn yellow" href="{{route('sell')}}">اعلن عن عربيتك</a>
     </div>
 </div>
+
 <div class="news flex flex-wrap flex-col justify-center items-center" style="margin: 90px 0">
     <p class="header">النشرة البريدية</p>
     <p class="text-center">لو انت تاجر أو بتحب تعرف اخر تتابع السوق النشرة هتعرفك آخر الأخبار أول بأول</p>
     <form class="w-full max-w-sm my-8">
         <div class="flex flex-wrap items-center py-2 px-4">
-            <input
-                class="appearance-none border border-indigo-700 bg-transparent text-gray-700 mr-3 py-2 px-2 leading-tight flex-grow w-full  md:w-auto md:ml-2 focus:outline-none"
-                type="text" placeholder="أدخل رقم الواتس" aria-label="whatsApp number" />
+            <input 
+            class="appearance-none border border-indigo-700 bg-transparent text-gray-700 mr-3 py-2 px-2 leading-tight flex-grow w-full md:w-auto md:ml-2 focus:outline-none" 
+            type="text" placeholder="أدخل رقم الواتس" aria-label="whatsApp number" />
             <button class="indigo mx-2 w-full block md:w-auto my-5 md:my-0 " type="button">اشترك الآن</button>
         </div>
     </form>
